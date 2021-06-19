ee13c39aa6e15c59f6737cd74cb23ae574822cef
AdrianNiewiadomski <kontakt@adrianniewiadomski.pl>
2021-06-19 16:14:10 +0200

    Searching and filtering by published date have been added.

--- app/controller.py
+++ app/controller.py
@@ -1,7 +1,8 @@
-from flask import render_template, request
-
+from flask import render_template, request, redirect, url_for
+import logging
 from . import app, db
 from .model import Book
+logging.basicConfig(level=logging.DEBUG)
 
 
 def select_all_books():
@@ -23,16 +24,49 @@ def initialize_database():
         insert_book("Rothfuss", "The Name of the Wind", 2007)
 
 
-def select_books_by_author(authors):
-    return Book.query.filter_by(authors=authors).all()
+# def select_books_by_authors(authors):
+#     return Book.query.filter_by(authors=authors).all()
+
+def select_books_by_published_date(published_date):
+    logging.debug(f"Filtering books by published date. ({published_date})")
+    return Book.query.filter_by(published_date=published_date).all()
+
+
+def sort_books_by_published_date(order):
+    if "-" in order:
+        logging.debug("Sorting books in descending order.")
+        return Book.query.order_by(Book.published_date.desc()).all()
+    logging.debug("Sorting books in ascending order.")
+    return Book.query.order_by(Book.published_date).all()
 
 
 @app.route("/books")
 def display_books():
-    if "author" in request.args.keys():
-        return render_template("index.html", books=select_books_by_author(request.args.get("author")))
+    if request.args:
+        logging.debug(f"\nThe following data has been received: {request.args}")
+        if "author" in request.args.keys():
+            logging.debug("author in request.args.keys()")
+            # return render_template("index.html", books=select_books_by_authors(request.args.get("author")))
+
+        elif "published_date" in request.args.keys():
+            logging.debug("published_date in request.args.keys()")
+            return render_template("index.html",
+                                   books=select_books_by_published_date(request.args.get("published_date")))
+
+        elif "sort" in request.args.keys():
+            logging.debug("sort in request.args.keys()")
+            return render_template("index.html", books=sort_books_by_published_date(request.args.get("sort")))
+
+        else:
+            logging.debug("else")
+            return redirect(url_for("display_books"))
     else:
         return render_template("index.html", books=select_all_books())
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return f"<h1>404</h1><p>{str(e)}</p>", 404
 
 
 def run():
